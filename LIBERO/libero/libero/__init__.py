537db30f54ccc982ad766d88202098a2e6b01d47
YY-GX <yyang9923@gmail.com>
2025-04-24 15:08:22 -0400

    update LIBERO to be replaced by boss44

--- LIBERO/libero/libero/__init__.py
+++ LIBERO/libero/libero/__init__.py
@@ -5,8 +5,7 @@ import yaml
 libero_config_path = os.environ.get(
     "LIBERO_CONFIG_PATH", os.path.expanduser("~/.libero")
 )
-config_file = os.path.join(libero_config_path, "config.yaml")
-
+config_file = os.path.join(libero_config_path, "config_boss.yaml")
 
 def get_default_path_dict(custom_location=None):
     if custom_location is None:
